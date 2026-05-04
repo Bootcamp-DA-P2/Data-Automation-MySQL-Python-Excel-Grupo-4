15e3a639a9e7fd8702519a219df20674ae276480
Ana Ganfornina <anies.llamani@gmail.com>
2026-05-04 13:59:14 +0200

    feat(main.py): Successfully connected to Sakila and performed the query for customer activity.

--- src/main.py
+++ src/main.py
@@ -20,30 +20,51 @@ def test_connection():
     try:
         with connection:
             print("Conexión exitosa a la base de datos.")
-            result = connection.execute(text("SELECT * FROM birds;"))
+            result = connection.execute(text("SELECT * FROM sakila;"))
             print(result.fetchone())
 
     except Exception as e:
         print(f"Error al conectar a la base de datos: {e}")
         
 
-def get_data_list_from_join():
+def get_data_list_from_join_activity_clients():
     """Obtener datos de la unión de tablas birds, locations y species"""
     connection = conection_bd()
     with connection:
-        join_query_sql = """ 
-                            SELECT
-                                sightings.date,
-                                sightings.count,
-                                birds.common_name AS species,
-                                parks.name AS park,
-                                parks.district AS district
-                            FROM
-                                sightings
-                            JOIN
-                                birds ON sightings.bird_id = birds.id
-                            JOIN
-                                parks ON sightings.park_id = parks.id;"""
+        join_query_sql = """
+                            select
+                                LOWER(cu.first_name) AS firt_name,
+                                LOWER(cu.last_name) AS last_name,
+                                LOWER(cu.email) AS email,
+                                cu.active,
+                                LOWER(ad.address) AS address,
+                                LOWER(ad.district) AS district,
+                                ad.postal_code, 
+                                ad.phone,
+                                LOWER(ci.city) AS city,
+                                LOWER(co.country) AS country,
+                                re.rental_date,
+                                re.return_date,
+                                pa.amount,
+                                pa.payment_date,
+                                DATEDIFF(re.return_date, re.rental_date) AS rental_duration
+                            from customer cu
+                            JOIN 
+                                address ad on cu.address_id = ad.address_id
+                            JOIN 
+                                city ci on ci.city_id = ad.city_id
+                            JOIN 
+                                country co on ci.country_id = co.country_id
+                            JOIN 
+                                rental re on cu.customer_id = re.customer_id
+                            JOIN 
+                                payment pa on re.rental_id = pa.rental_id
+                            WHERE
+                                rental_date IS NOT NULL
+                                AND return_date IS NOT NULL
+                                AND amount > 0
+                                AND rental_date < return_date;
+                            """
     
         result = connection.execute(text(join_query_sql))
         rows = result.fetchall()
@@ -54,15 +75,15 @@ def get_data_list_from_join():
             
             # --- 3. EXPORTAR A CSV (Paso Nuevo) ---
         df.to_csv(
-                "data/madrid_bird_sightings.csv", # ruta y Nombre del archivo de salida
+                "data/sakila.csv", # ruta y Nombre del archivo de salida
                 index=False, # Evita escribir el índice del DataFrame en el archivo
                 encoding='utf-8' # Asegura que caracteres especiales (como acentos) se guarden bien
         )
 
-        print(f"✅ DataFrame successfully created and saved to: {'data/madrid_bird_sightings.csv'}")
+        print(f"✅ DataFrame successfully created and saved to: {'data/sakila.csv'}")
 
         return df
         
 if __name__ == "__main__":
     test_connection()
-    get_data_list_from_join()
+    get_data_list_from_join_activity_clients()
