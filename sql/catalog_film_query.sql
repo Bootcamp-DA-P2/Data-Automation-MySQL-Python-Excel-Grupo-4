d6170c36fbe138a60fd3578e9f966d0805756eed
Ana Ganfornina <anies.llamani@gmail.com>
2026-05-04 15:02:47 +0200

    wip(catalog_film_query): clean film data and aggregate inventory stock

---
+++ sql/catalog_film_query.sql
@@ -0,0 +1,22 @@
+use sakila;
+
+SELECT 
+	LOWER(TRIM(title)) AS title,
+	LOWER(TRIM(description)) AS description,
+	release_year,
+	rental_duration,
+	rental_rate,
+	length,
+	replacement_cost,
+	rating,
+    LOWER(TRIM(cat.name)) AS category,
+    LOWER(TRIM(l.name)) AS language,
+    COUNT(inv.inventory_id) AS total_inventory
+FROM film
+JOIN film_category fcat ON fcat.film_id = film.film_id
+JOIN category cat ON fcat.category_id = cat.category_id
+JOIN language l ON l.language_id = film.language_id
+JOIN inventory inv ON film.film_id = inv.film_id
+-- GROUP BY film.title, cat.name;
+
+
