3ff9979ef9f5eeb1a1ce1d2b2df343e7bf6d48be
Ana Ganfornina <anies.llamani@gmail.com>
2026-05-05 08:35:53 +0200

    feat(catalog_film_query): catalog_film query

--- sql/catalog_film_query.sql
+++ sql/catalog_film_query.sql
@@ -17,6 +17,18 @@ JOIN film_category fcat ON fcat.film_id = film.film_id
 JOIN category cat ON fcat.category_id = cat.category_id
 JOIN language l ON l.language_id = film.language_id
 JOIN inventory inv ON film.film_id = inv.film_id
--- GROUP BY film.title, cat.name;
+GROUP BY 
+	film.title, 
+    film.description,
+    film.release_year,
+    film.rental_duration,
+    film.rental_rate,
+    film.length,
+    film.replacement_cost,
+    film.rating,
+    cat.name,
+    l.name;
+    
+    
 
 
