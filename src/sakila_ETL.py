92dcc82bf76ec2834d938aaae16fe54b8f06e670
Ana Ganfornina <anies.llamani@gmail.com>
2026-05-08 14:24:41 +0200

    feat(main,sakila_ELT): function to make the sql queryes

--- src/sakila_ETL.py
+++ src/sakila_ETL.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 BASE_PATH = "output/"
-paths = ['client_activity_rental.csv', '.csv', 'global_activity_clients.csv']
+paths = ['client_activity_rental.csv', 'catalog_film.csv', 'global_activity_clients.csv']
 
 query_sql_1 = """
                 select
@@ -37,6 +37,27 @@ query_sql_1 = """
             """
 
 sql_query_2 = """
+                USE sakila;
+                SELECT 
+                    LOWER(TRIM(film.title)) AS title,
+                    LOWER(TRIM(cat.name)) AS category,
+                    length,
+                    rating,
+                    LOWER(TRIM(l.name)) AS language,
+                    COUNT(DISTINCT inv.inventory_id) AS total_inventory, -- Contamos cuántas copias físicas distintas existen para esta película
+                    COUNT(r.rental_id) AS total_rentals -- Contamos cuántas transacciones de alquiler existen en total
+                FROM film
+                JOIN film_category fcat ON fcat.film_id = film.film_id
+                JOIN category cat ON fcat.category_id = cat.category_id
+                JOIN language l ON l.language_id = film.language_id
+                JOIN inventory inv ON film.film_id = inv.film_id
+                JOIN rental r ON inv.inventory_id = r.inventory_id
+                GROUP BY 
+                    film.title,
+                    cat.name,
+                    l.name,
+                    film.length,
+                    film.rating;
 
 """
 
