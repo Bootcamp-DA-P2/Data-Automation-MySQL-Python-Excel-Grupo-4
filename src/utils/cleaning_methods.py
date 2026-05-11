45824097599253d57d29e821808ac86b7f06e168
Daniel Luque <daniel.luquegallardo@gmail.com>
2026-05-11 13:36:43 +0200

    fix(notebooks, utils): usinf methods to refactorize de data preprocess

--- src/utils/cleaning_methods.py
+++ src/utils/cleaning_methods.py
@@ -1,53 +1,52 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #charge the path
 def charge_path(path):
     df = pd.read_csv(path)
     return df
 
-def first_visualizer(df):
-    # Visualización de las primeras filas del dataframe
-    print(df.head())
-    # Visualización del tamaño del dataframe
-    print(f'El dataset tiene {df.shape[1]} columnas')
-    print(f'El dataset tiene {df.shape[0]} filas/registros')
-    print("-------------------------------")
-    print(f'El dataset tiene {df.duplicated().sum()} filas duplicadas.')
-    print("-------------------------------")
-    # Suma de valores nulos por columna
-    print("Suma de valores nulos por columna:")
-    print(df.isnull().sum())
-    print("-------------------------------")
-    # Porcentaje de valores nulos por columna
-    print("Porcentaje de valores nulos por columna:")
-    print(round(df.isnull().sum() * 100 / len(df),2))
-    print("-------------------------------")
-    # Tipos de datos de cada columna
-    print("Tipos de datos de cada columna:")   
-    print(df.info())
-    print("-------------------------------")
-    # Estadísticas descriptivas para columnas numéricas
-    print("Estadísticas descriptivas para columnas numéricas:")
-    print(df.describe())
-    print("-------------------------------")
-    # Estadísticas descriptivas para columnas categóricas
-    print("Estadísticas descriptivas para columnas categóricas:")
-    print(df.describe(include='object'))
+def show_head(df, top_n=10):
+    return df.head(top_n)
 
+def show_info(df):
+    return df.info()
+
+def first_visualizer(df):
+
+    # Visualización del tamaño del dataframe
+    num_columnas = df.shape[1]
+    num_filas = df.shape[0]
+
+    #Duplicados
+    num_duplicados = df.duplicated().sum()
+
+    # Suma de valores nulos por columna
+    null_values = df.isnull().sum().sum()
+
+    # Porcentaje de valores nulos por columna
+    df_porcentaje_nulos_columna = (df.isnull().sum() * 100 / len(df),2)
+
+
+    # Estadísticas descriptivas para columnas categóricas
+    df_describe_numeric = df.describe()
+    # Estadísticas descriptivas para columnas categóricas
+    df_describe_categorical = df.describe(include='object')
+
+    return num_columnas, num_filas, num_duplicados, null_values, df_porcentaje_nulos_columna, df_describe_numeric, df_describe_categorical
 
 def normalize_string_columns(df):
     """
     Normalización de las columnas de tipo string: convertir a minúsculas, eliminar espacios y caracteres especiales.
     """
-
     columns_str = df.select_dtypes("object").columns
 
     for column in columns_str:
         df[column] = df[column].astype("string").str.lower().str.strip()
         df[column] = df[column].str.replace(r'[^a-zA-Z0-9 @]', '', regex=True)
 
-    print(df.info())
+    return df
 
 def normalize_date_columns(df):
     """
@@ -56,6 +55,14 @@ def normalize_date_columns(df):
     column_date = df.filter(like='date').columns
     for column in column_date:
         df[column] = pd.to_datetime(df[column])
+    return df
+
+def delete_columns(df, columns_to_delete):
+    """
+    Eliminar columnas específicas del dataframe.
+    """
+    df = df.drop(columns=columns_to_delete, axis=1)
+    return df
 
 
 def delete_nulls(df):
@@ -66,32 +73,35 @@ def delete_nulls(df):
     print("En este caso no se encuentran valores nulos por lo que no se procede a eliminar filas.")
 
 
-def identify_outliers(df):
+def identify_outliers(df, variables_numericas, variables_categoricas):
     """
     Identificación de valores atípicos en columnas numéricas utilizando el método del rango intercuartílico (IQR).
     """
-    print("Identificación de valores atípicos en columnas numéricas:")
-    numeric_columns = df.select_dtypes(include=['number']).columns
-    for column in numeric_columns:
-        Q1 = df[column].quantile(0.25)
-        Q3 = df[column].quantile(0.75)
-        IQR = Q3 - Q1
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
-        print(f"Columna: {column}, Número de outliers: {outliers.shape[0]}")
-
-    # Visualización de histogramas y boxplots para cada columna numérica
-    for column in numeric_columns:
-        plt.figure(figsize=(10, 5))
-        plt.hist(df[column], bins=15, edgecolor='black')
-        plt.title(f'Histogram column: {column}')
-        plt.xlabel(column)
+    for col in variables_numericas:
+        plt.figure(figsize=(8,4))
+        sns.histplot(df[col], kde=True)
+        plt.title(f'Histograma de {col}')
+        plt.xlabel(col)
         plt.ylabel('Frecuencia')
         plt.show()
 
-        plt.boxplot(df[column], orientation='horizontal')
-        plt.title(f'Boxplot column: {column}')
+        plt.figure(figsize=(8,4))
+        sns.boxplot(x=df[col], orient='horizontal')
+        plt.title(f'Boxplot de {col}')
+        plt.xlabel(col)
+        plt.show()
+    
+    for col in variables_categoricas:
+        plt.figure(figsize=(8,4))
+        sns.countplot(x=df[col])
+        plt.title(f'Conteo de {col}')
+        plt.xlabel(col)
+        plt.ylabel('Frecuencia')
+        plt.xticks(rotation=45)
+        plt.show()
+
+def load_clean_data(df, path):
+    df.to_csv(path, index=False)
 
 # Función de limpieza para el segundo dataframe
 def preprocess_df(path):
