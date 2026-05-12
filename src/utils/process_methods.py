123daf0a4dcdbee342db557a0c890b5b28bd7d88
Daniel Luque <daniel.luquegallardo@gmail.com>
2026-05-12 10:16:11 +0200

    feat(utils): third method to process the data added

--- src/utils/process_methods.py
+++ src/utils/process_methods.py
@@ -56,4 +56,54 @@ def preprocess_first_df(df):
     for column in columns_str:
         df[column] = df[column].fillna("missing")
 
+    # Exportación de los datos procesados
     load_clean_data(df, '../output/client-activity-clean.csv')
+
+
+def preprocess_third_df(df):
+    df.columns = df.columns.str.lower().str.strip().str.replace(' ', '_')
+
+    # Normalización a tipo fecha y string
+    df = normalize_date_columns(df)
+    df = normalize_string_columns(df)
+
+    # Creación de columnas adicionales a partir de las fechas
+    df['rental_day'] = df['rental_date'].dt.day
+    df['rental_month'] = df['rental_date'].dt.month
+    df['rental_year'] = df['rental_date'].dt.year
+    df['rental_hour'] = df['rental_date'].dt.hour
+    df['return_day'] = df['return_date'].dt.day
+    df['return_month'] = df['return_date'].dt.month
+    df['return_year'] = df['return_date'].dt.year
+    df['return_hour'] = df['return_date'].dt.hour
+
+    # Creación de la columna 'day_of_week_rental' para indicar el día de la semana en el que se realizó el alquiler
+    df['day_of_week_rental'] = df['rental_date'].dt.dayofweek + 1
+
+    # Creación de columna 'is_weekend_rental' para indicar si el alquiler se realizó en fin de semana (1) o no (0)
+    df['is_weekend_rental'] = df['day_of_week_rental'].apply(lambda x: 1 if x in [6, 7] else 0)
+
+    # Creación de la columna 'season' para indicar la estación del año en el que se realizó el alquiler
+    df['season'] = df['rental_month'].apply(lambda x: 'spring' if x in [3,4,5] else ('summer' if x in [6,7,8] else ('autumn' if x in [9,10,11] else 'winter')))
+
+    # Creación de la columna 'part_of_the_day' para indicar el tramo del día en el que se realizó el alquiler
+    df['rental_hour_part_of_the_day'] = df['rental_hour'].apply(lambda x: 'night' if x in [0,1,2,3,4,5,6] else('morning' if x in [7,8,9,10,11,12] else 'afternoon'))
+
+    # Creación de la columna 'part_of_the_day' para indicar el tramo del día en el que se realizó la devolución
+    df['return_hour_part_of_the_day'] = df['return_hour'].apply(lambda x: 'night' if x in [0,1,2,3,4,5,6] else('morning' if x in [7,8,9,10,11,12] else 'afternoon'))
+
+    # Creación de la columna 'payment_lag' para indicar el tiempo transcurrido entre la fecha de alquiler y la fecha de pago. Lo ideal es que sea 0,
+    # pero puede haber casos en los que el pago se realice después del alquiler. Sirve para detectar posibles anomalías
+    df['payment_lag'] = df['payment_date'].dt.day - df['rental_date'].dt.day
+
+    # Creación de la columna 'film_age' para indicar la antigüedad de la película desde su año de lanzamiento.
+    df['film_age'] = dt.now().year - df['release_year']
+
+    # Creación de la columna 'is_adult_content' para indicar si la película es para adultos o no, basándonos en su clasificación por edades
+    df['is_adult_content'] = df['rating'].apply(lambda x: 1 if x in ['R', 'NC-17'] else 0)
+
+    # Eliminación de las columnas redundantes tras la creación de columnas adicionales a pertir de ellas
+    df = delete_columns(df, ['release_year', 'rental_date', 'return_date'])
+
+    # Exportación de los datos procesados
+    load_clean_data(df, '../output/global_activity_clients_clean.csv')
